27064141e814c964ced6aac21f0c30a0a3f1129b
nikolasmish <nikola.smis@outlook.com>
2023-02-28 19:59:27 +0100

    add colorscheme match paren color

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -4,5 +4,6 @@ if (not status) then return end
 n.setup({
   highlight_overrides = {
     Search = { bg = '#82aaff', fg = '#333333' },
+    MatchParen = { bg = '#a6accd', fg = '#333333' },
   },
 })
