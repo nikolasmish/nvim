0eb35647bbbc30fcd1162c78cd4b46b29e36cf8a
nikolasmish <nikola.smis@outlook.com>
2023-03-13 01:28:18 +0100

    add nvim tree follow file

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -2,6 +2,10 @@ local status, nvim_tree = pcall(require, 'nvim-tree')
 if (not status) then return end
 
 nvim_tree.setup({
-  
-})
+  update_focused_file = {
+    enable = true,
+    update_root = false,
+    ignore_list = {},
+  },
 
+})
