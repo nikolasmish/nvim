ce22ebe343cfa36f54047a29521ed2f621fbe428
Nikola Simic <nikola.smis@outlook.com>
2023-10-18 22:30:05 +0200

    test prettierd format speed

---
+++ lua/plugins/darknvim.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "sainnhe/gruvbox-material",
+    name = "gruvbox-material",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd("colorscheme gruvbox-material")
+    end,
+  },
+}
