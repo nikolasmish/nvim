60fd2bd62b214288842de2d34b6ef15febb95ba5
Nikola Simic <nikola.simic@teletrader.local>
2024-01-08 20:53:23 +0100

    add autopair, change colorscheme

--- lua/plugins/darknvim.lua
+++ lua/plugins/darknvim.lua
@@ -12,4 +12,12 @@ return {
       vim.cmd("colorscheme kanagawa")
     end,
   },
+  {
+    "Shatur/neovim-ayu",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      vim.cmd("colorscheme ayu-dark")
+    end,
+  },
 }
