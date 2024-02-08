0ccb1d42e891909d1f2c742759ba646a10818792
Nikola Simic <nikola.simic@teletrader.local>
2024-02-08 13:10:16 +0100

    remove kanagawa and ayu on startup

--- lua/plugins/darknvim.lua
+++ lua/plugins/darknvim.lua
@@ -8,16 +8,10 @@ return {
     name = "kanagawa",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd("colorscheme kanagawa")
-    end,
   },
   {
     "Shatur/neovim-ayu",
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      vim.cmd("colorscheme ayu-dark")
-    end,
   },
 }
