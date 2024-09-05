545eb5dd792b62813c1251b2f126123e5734ebf5
nikola.simic <nikola.simic@teletrader.com>
2024-09-05 10:02:53 +0200

    add markdown preview, update for nvim 0.10

--- lua/plugins/markdown_preview.lua
+++ lua/plugins/markdown_preview.lua
@@ -1,7 +1,8 @@
 return {
   {
     "iamcco/markdown-preview.nvim",
-    event = "BufRead",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
     build = function()
       vim.fn["mkdp#util#install"]()
     end,
