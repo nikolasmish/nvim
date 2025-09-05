6cecf25e617ca1ecc0ea84d2394e560e6a457980
nikola.simic <nikola.simic@teletrader.com>
2025-09-05 11:45:39 +0200

    add neogit and diffview

--- lua/plugins/darknvim.lua
+++ lua/plugins/darknvim.lua
@@ -11,4 +11,11 @@ return {
     --   vim.cmd("colorscheme rose-pine-moon") -- OR vim.cmd("colorscheme shin")
     -- end,
   },
+  { "bluz71/vim-moonfly-colors", name = "moonfly", lazy = false, priority = 1000 },
+  {
+    "Koalhack/darcubox-nvim",
+    config = function()
+      vim.cmd("colorscheme darcubox")
+    end,
+  },
 }
